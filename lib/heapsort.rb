3d164d8c5c13823fba523da34d5151712e743166
jawisniewski <wisninho09@gmail.com>
2016-11-28 10:48:23 +0100

    Add files via upload

--- lib/heapsort.rb
+++ lib/heapsort.rb
@@ -1,82 +1,71 @@
 public def Rozdziel (childArray)
-$czy_ponownie = 1
-childArray.heapsort!
-
+  $czy_ponownie = 1
+  childArray.heapsort!
 end
-  public def heapsort!
-
-j=0
-
-while j<self.length
-	if(self[j].is_a?(Array)) then
-Rozdziel self[j]
-end
-j +=1
-end
-
-    #heapify
+public def heapsort!
+  j=0
+  while j<self.length
+    if(self[j].is_a?(Array)) then
+    Rozdziel self[j]
+    end
+    j +=1
+  end
     1.upto(self.length - 1) do |i|
-      #move up
-
       child = i
       while child > 0
-
-
         parent = (child - 1) / 2
-if (self[parent].is_a?(Array) ) then
-	break
-else if( self[child].is_a?(Array)) then
-self[child], self[parent] = self[parent], self[child]
-child = parent
-else
-        if self[parent] < self[child]  
-          self[parent], self[child] = self[child], self[parent]
+        if (self[parent].is_a?(Array) ) then
+          break
+        else if( self[child].is_a?(Array)) then
+          self[child], self[parent] = self[parent], self[child]
           child = parent
         else
-          break
-end
-end
+          if self[parent] < self[child]
+            self[parent], self[child] = self[child], self[parent]
+            child = parent
+          else
+            break
+          end
         end
-      end
-    end
-
-    #sort
-
-    i = self.length - 1
-    while i > 0
-      self[0], self[i] = self[i], self[0]
-      i -= 1
-      #move down
-      parent = 0
-      while parent * 2 + 1 <= i
- 	child = parent * 2 + 1
-if (self[parent].is_a?(Array)) then
-break
-else
-if (child <i and self[child+1].is_a?(Array)) then
-if ( self[child].is_a?(Array)) then
-else
- child += 1
-end
-end
-if( self[child].is_a?(Array)) then
-          self[child], self[parent] = self[parent], self[child]
-parent = child
-else
-        if child < i && self[child] < self[child + 1]
-          child += 1
-        end
-        if self[parent] < self[child]
-          self[parent], self[child] = self[child], self[parent]
-          parent = child
-        else
-          break
-        end
-end
-end
       end
     end
   end
+#sort
+  i = self.length - 1
+  while i > 0
+    self[0], self[i] = self[i], self[0]
+    i -= 1
+  #move down
+    parent = 0
+    while parent * 2 + 1 <= i
+ 	    child = parent * 2 + 1
+      if (self[parent].is_a?(Array)) then
+        break
+      else
+        if (child <i and self[child+1].is_a?(Array)) then
+          if ( self[child].is_a?(Array)) then
+          else
+            child += 1
+          end
+        end
+        if( self[child].is_a?(Array)) then
+          self[child], self[parent] = self[parent], self[child]
+          parent = child
+        else
+          if child < i && self[child] < self[child + 1]
+            child += 1
+          end
+          if self[parent] < self[child]
+            self[parent], self[child] = self[child], self[parent]
+            parent = child
+          else
+            break
+          end
+        end
+      end
+    end
+  end
+end
 
 
 
