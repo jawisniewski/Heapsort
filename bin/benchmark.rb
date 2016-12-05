37be3f759f97bce9faa7ffe8bbd5fdf98ce11752
jawisniewski <wisninho09@gmail.com>
2016-12-05 01:48:25 +0100

    Add files via upload

--- bin/benchmark.rb
+++ bin/benchmark.rb
@@ -11,8 +11,8 @@ rand3 = rand1, rand2
   rand5 = 400000.times.map { ('a'..'z').to_a.shuffle[0..25].join}
 Benchmark.bm do |x|
 
-          x.report('heapsort string 4000 [0-4]') { rand5.dup.heapsort! }
-        x.report('heapsort string 400000 [0..25]') { rand4.dup.heapsort! }
+          x.report('heapsort string 4000 [0-4]') { rand4.dup.heapsort! }
+        x.report('heapsort string 400000 [0..25]') { rand5.dup.heapsort! }
   x.report('heapsort  200000') { rand1.dup.heapsort! }
   x.report('heapsort 500000') { rand2.dup.heapsort! }
     x.report('heapsort table [rand1],[rand2]') { rand3.dup.heapsort! }
